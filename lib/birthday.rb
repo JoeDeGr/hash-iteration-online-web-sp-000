5ac4ec0a175fa6f2edc6f89c0a8ba778ad26d844
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-14 00:23:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,11 +12,6 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |k, v|
-    if v > 12
-      puts "You are too old for this."
-    else
-      puts "Happy Birthday #{k}! You are now #{v} years old!"
-    end
+  birthday_kids.each {|k, v| v > 12 ? puts "You are too old for this." : puts "Happy Birthday #{k}! You are now #{v} years old!"}
   end
 end
